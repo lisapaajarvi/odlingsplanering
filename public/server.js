9ea2052b9dc299b05af991121d9ba540510f3937
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 16:34:59 +0200

    basic server functions

---
+++ public/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static('public'))
+
+
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
