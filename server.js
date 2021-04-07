d8c855c5cc76953cf500d8e3d84299346f2e148d
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 20:16:07 +0200

    added endpoints

--- server.js
+++ server.js
@@ -17,13 +17,11 @@ let chores = [
         "time-est": 30,
         "due-date": "2021-04-15"
     }
-
 ]
 
 app.use(express.static('public'));
+
 app.use(express.json());
-
-
 
 app.get('/api/chores', (req,res) => {
     res.json(chores);
@@ -34,6 +32,17 @@ app.post('/api/chores', (req,res) => {
     res.status(201).json(req.body);
 });
 
+app.delete('/api/chores', (req,res) => {
+    const index = chores.findIndex(chore => chore.id === req.body.id);
+    const deletedChore = chores.splice(index, 1);
+    res.json(deletedChore);
+})
+
+app.put('/api/chores', (req,res) => {
+    const index = chores.findIndex(chore => chore.id === req.body.id);
+    const editedChore = chores.splice(index, 1, req.body);
+    res.json(editedChore);
+})
 
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
