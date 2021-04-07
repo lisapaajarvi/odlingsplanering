f4456e77f04d9161f464620ef670903aca9e57f9
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 23:36:20 +0200

    added form

--- public/script.js
+++ public/script.js
@@ -2,7 +2,10 @@ window.addEventListener("load", main)
 
 function main() {
     getChores();
+    const submitButton = document.getElementById("submit")
+    submitButton.addEventListener("click", addNewChore)
 }
+
 async function getChores() {
     const response = await fetch('/api/chores');
     const chores = await response.json();
@@ -10,19 +13,24 @@ async function getChores() {
     for (chore of chores) {
         const choreDiv = document.createElement('div'); 
         choreDiv.className = "choreDiv";
+
         const choreTitle = document.createElement('h4'); 
         choreTitle.innerHTML = chore.title;
         choreDiv.appendChild(choreTitle)
+
         const choreTime = document.createElement('p'); 
         choreTime.innerHTML = "Uppskattad tidsåtgång: " + chore.time + " min";
         choreDiv.appendChild(choreTime)
+
         const choreDate = document.createElement('p'); 
         choreDate.innerHTML = "Slutdatum: " + chore.date;
         choreDiv.appendChild(choreDate)
+
         choreContainer.appendChild(choreDiv);
     }
+}
 
-    //chores.forEach(createChoreDiv)
-    //console.log(chores);
+function addNewChore() {
+    
 }
 
