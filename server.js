626bcdc2c6ff75b108a3279fe79634f54f2c316b
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 19:51:37 +0200

    added rest

--- server.js
+++ server.js
@@ -2,10 +2,37 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+let chores = [
+    {
+        "id": 1,
+        "category": "Rensning",
+        "title": "Rensa ogräs i bädd A2",
+        "time-est": 30,
+        "due-date": "2021-04-10"
+    },
+    {
+        "id": 2,
+        "category": "Sådd",
+        "title": "Så lupiner i bädd A2",
+        "time-est": 30,
+        "due-date": "2021-04-15"
+    }
+
+]
+
+app.use(express.static('public'));
 app.use(express.json());
 
-app.use(express.static('public'))
 
+
+app.get('/api/chores', (req,res) => {
+    res.json(chores);
+});
+
+app.post('/api/chores', (req,res) => {
+    chores.push(req.body);
+    res.status(201).json(req.body);
+});
 
 
 
