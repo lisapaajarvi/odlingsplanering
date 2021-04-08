ccaacb5d6cca70595be352369964b3212edaed1b
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-08 11:30:46 +0200

    added edit function and form render with js

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", main)
 function main() {
     getChores();
     const submitButton = document.getElementById("submit")
-    submitButton.addEventListener("click", addNewChore)
+    submitButton.addEventListener("click", submitChore)
 }
 
 async function getChores() {
@@ -26,19 +26,116 @@ async function getChores() {
         choreDate.innerHTML = "Slutdatum: " + chore.date;
         choreDiv.appendChild(choreDate)
 
+        choreDiv.addEventListener("click", editChore.bind(chore))
+
         choreContainer.appendChild(choreDiv);
     }
 }
 
-async function addNewChore() {
-    const choreId = document.getElementById("id").value;
+function renderForm(chore) {
+    const formContainer = document.getElementById("form");
+    formContainer.innerHTML = '';
+
+    const formTitle = document.createElement('h2'); 
+        if(chore) {
+            formTitle.innerHTML = "Ändra uppgift"; 
+        }
+        else {
+            formTitle.innerHTML = "Lägg till uppgift"; 
+        }
+    formContainer.appendChild(formTitle)
+
+    const titleInput = document.createElement('textarea'); 
+    titleInput.type = "text";
+    titleInput.className = "input";
+    titleInput.id = "title"
+    if(chore) {
+        titleInput.value = chore.title
+    }
+    else {
+        titleInput.placeholder = "Fyll i uppgiftens titel"
+    }
+    formContainer.appendChild(titleInput);
+
+    const timeInput = document.createElement('input'); 
+    timeInput.type = "number";
+    timeInput.className = "input";
+    timeInput.id = "time"
+    if(chore) {
+        timeInput.value = chore.time
+    }
+    else {
+        timeInput.placeholder = "Fyll i uppskattad tid (min)"     
+    }
+    formContainer.appendChild(timeInput);
+
+    const dateInput = document.createElement('input'); 
+    dateInput.type = "date";
+    dateInput.className = "input";
+    dateInput.id = "date"
+    if(chore) {
+        dateInput.value = chore.date
+    }
+    formContainer.appendChild(dateInput);
+
+    const submitButton = document.createElement('button');
+    submitButton.type = "submit";
+    submitButton.className = "input";
+    submitButton.id = "submit";
+    if(chore) {
+        submitButton.innerHTML = "Spara ändringar";
+    }
+    else {
+        submitButton.innerHTML = "Lägg till";
+    }
+    submitButton.addEventListener("click", submitChore.bind(chore))
+    formContainer.appendChild(submitButton);
+
+    if(chore) {
+        const cancelButton = document.createElement('button');
+        cancelButton.type = "submit";
+        cancelButton.className = "input";
+        cancelButton.innerHTML = "Avbryt";
+        cancelButton.addEventListener("click", cancelChanges);
+        formContainer.appendChild(cancelButton);
+    }
+}
+
+function cancelChanges() {
+    renderForm();
+}
+
+function editChore() {
+    renderForm(this)
+}
+
+function editChoreInAPI(newChore) {
+    console.log("ändra i API")
+}
+
+function submitChore() {
+    let choreId = 0;
+    if(this) {
+        choreId = this.id
+    }
+    else {
+        choreId = Math.floor(Math.random() * 1000);
+    }
+
     const choreTitle = document.getElementById("title").value;
     const choreTime = document.getElementById("time").value;
     const choreDate = document.getElementById("date").value;
-
     const newChore = {id: choreId, title: choreTitle, time: choreTime, date: choreDate};
-    
 
+    if(this) {
+        editChoreInAPI(newChore)
+    }
+    else {
+        addNewChoreToAPI(newChore)
+    }
+}
+async function addNewChoretoAPI(newChore) {
+    console.log("lägg till nytt")
     const response = await fetch('/api/chores', {
         method: 'POST',
         headers: {
